19ba53be1a0747b5f0cb6701690f378f430cfc59
Kavleer <chriskabler@yahoo.com>
2019-11-22 15:18:03 -0500

    add charts

--- intervals/static/main.js
+++ intervals/static/main.js
@@ -3,25 +3,25 @@ var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Ma2', 'Ma3', 'P4', 'P5', 'Mi6', 'Octave'],
         datasets: [{
-            label: '# of Votes',
+            label: '# of attempts',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 99, 132, 0.4)',
+                'rgba(255, 99, 132, 0.6)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(75, 192, 192, 0.4)',
+                'rgba(75, 192, 192, 0.6)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)'
             ],
             borderWidth: 1
         }]
