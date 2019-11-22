5cb6699b28f185fee28ae3f738b9d1f8f92e4455
Bitha Elias <bitha.jordan@gmail.com>
2019-11-22 11:54:24 -0500

    adds js and links registration button

--- intervals/static/main.js
+++ intervals/static/main.js
@@ -1,7 +1,2 @@
 
-// document.image.classList.add('fade');
 
-// document.addEventListener("DOMContentLoaded", function (e) {
-//     console.log('hello')
-//     document.image.classList.remove('fade');
-// });
