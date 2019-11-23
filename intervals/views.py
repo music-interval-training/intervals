92f69354b3797b3326d3c7dd4ac69ac2a4b32cab
lesliehealray <lesliehealray@gmail.com>
2019-11-23 18:07:09 -0500

    added noIntent

--- intervals/views.py
+++ intervals/views.py
@@ -99,7 +99,8 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input)
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input) or
+                ask_utils.is_intent_name("noIntent")(handler_input)
                 )
 
     def handle(self, handler_input):
