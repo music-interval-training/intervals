fd9761e02407259a9a07e895cfd1becca67ec063
lesliehealray <lesliehealray@gmail.com>
2019-11-23 17:20:27 -0500

    minor changes for testing

--- intervals/views.py
+++ intervals/views.py
@@ -45,7 +45,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
       
         return (
                 handler_input.response_builder
-                    .speak(speak_output)
+                    .ask(speak_output)
                     .response
             )
 
