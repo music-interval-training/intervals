5475e6df30dc22ad50c0b6b29968e1a3338f4f27
lesliehealray <lesliehealray@gmail.com>
2019-11-23 19:07:33 -0500

    configured database to utilize postgres and initial models.py

--- intervals/models.py
+++ intervals/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    interval = models.CharField(max_length=30)
+    guess = models.CharField(max_length=30)
+    audio = models.URLField()
+
+    @property
+    def is_correct(self):
+        return self.interval == self.guess 
+
+    
+
+
+
