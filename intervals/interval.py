82a3e5f587502a044f1f189ea72a390af08af88f
Bitha Elias <bitha.jordan@gmail.com>
2019-11-26 14:43:09 -0500

    fixes bug from merge conflict

--- intervals/interval.py
+++ intervals/interval.py
@@ -1,6 +1,20 @@
 from random import randrange
 
-def get_audio_file():
+MAJOR_SECOND = ['major 2nd', 'major second']
+MINOR_SECOND = ['minor 2nd', 'minor second']
+MAJOR_THIRD = ['major 3rd', 'major third']
+MINOR_THIRD = ['minor 3rd', 'minor third']
+PERFECT_FOURTH = ['perfect 4th', 'perfect fourth']
+PERFECT_FIFTH = ['perfect 5th', 'perfect fifth']
+MAJOR_SIXTH = ['major 6th', 'major sixth']
+MINOR_SIXTH = ['minor 6th', 'minor sixth']
+MINOR_SEVENTH = ['minor 7th', 'minor seventh']
+MAJOR_SEVENTH = ['major 7th', 'major seventh']
+OCTAVE = ['octave']
+TRITONE = ['tritone']
+
+
+def get_audio_info():
     intervals =[
         {"interval": MAJOR_SECOND, "audio_files": ["Major_2-A-B+.mp3", "Major_2-D-E+.mp3", "Major_2-G%23-A%23.mp3"]},
         {"interval": MINOR_SECOND, "audio_files": ["Minor_2-A-B-flat+.mp3", "Minor_2-C-C%23+.mp3", "Minor_2-F-F%23+.mp3"]},
@@ -14,7 +28,6 @@ def get_audio_file():
         {"interval": MINOR_SEVENTH, "audio_files": ["Minor_7-E-D.mp3", "Minor_7-E-flat-D-flat.mp3", "Minor_7-F-E-flat.mp3"]}, 
         {"interval": MAJOR_SEVENTH, "audio_files": ["Major_7-C-B.mp3", "Major_7-E-flat-D.mp3", "Major_7-F-E.mp3"]}, 
         {"interval": OCTAVE, "audio_files": ["Octave_C-C.mp3", "Octave_D-D.mp3", "Octave_E-E.mp3"]}, 
-
     ]
 
     idx = randrange(0, len(intervals))
@@ -23,5 +36,5 @@ def get_audio_file():
     audio = interval['audio_files']
     random_file = audio[audio_idx]
     url = f"https://cohort-6-intervals.s3.amazonaws.com/{random_file}" 
-    return url
+    return (interval['interval'], url)
 
--- intervals/views.py
+++ intervals/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 
-from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.handler_input import HandlerInput
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
+import logging
 from ask_sdk_core.utils import is_request_type
-from ask_sdk_model import Response
+import ask_sdk_core.utils as ask_utils
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import (
+    AbstractRequestHandler, AbstractExceptionHandler,
+    AbstractResponseInterceptor, AbstractRequestInterceptor)
+from ask_sdk_core.handler_input import HandlerInput
 from django.views.decorators.csrf import csrf_exempt
-from .interval import get_audio_file
+from ask_sdk_model import Response
+
+from .interval import get_audio_info
+from .models import Record
 
 sb = SkillBuilder()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def home_page(request):
     return render(request, "intervals/home_page.html")
 
 def progress_details(request):
-
     Records = Record.objects.all()
     return render(request, "intervals/progress_details.html", {
         'Records': Records,
@@ -100,10 +109,112 @@ class HelpIntentHandler(AbstractRequestHandler):
                 .response
         )
 
-# Other skill components here ....
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input) or
+                ask_utils.is_intent_name("noIntent")(handler_input)
+                )
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Goodbye!"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .response
+        )
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = "You just triggered " + intent_name + "."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+# Request and Response Loggers
+class RequestLogger(AbstractRequestInterceptor):
+    """Log the request envelope."""
+    def process(self, handler_input):
+        # type: (HandlerInput) -> None
+        logger.info("Request Envelope: {}".format(
+            handler_input.request_envelope))
+
+
+class ResponseLogger(AbstractResponseInterceptor):
+    """Log the response envelope."""
+    def process(self, handler_input, response):
+        # type: (HandlerInput, Response) -> None
+        logger.info("Response: {}".format(response))
+
 
 # Register all handlers, interceptors etc.
 sb.add_request_handler(LaunchRequestHandler())
+# sb.add_request_handler(StartTrainingIntentHandler())
+sb.add_request_handler(IntervalGuessIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+# sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+sb.add_exception_handler(CatchAllExceptionHandler())
+# Add response interceptor to the skill.
+sb.add_global_request_interceptor(RequestLogger())
+sb.add_global_response_interceptor(ResponseLogger());
+
 
 skill = sb.create()
 
