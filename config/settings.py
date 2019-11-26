c797a743f7a03fc8de439d6cf18b9b4791ed1bbd
lesliehealray <lesliehealray@gmail.com>
2019-11-25 20:41:17 -0500

    modified debug to evaluate to boolean

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == 'True'
 
 ALLOWED_HOSTS = ['www.musicintervaltraining.com', '127.0.0.1']
 
