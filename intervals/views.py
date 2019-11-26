f9936cacefeab5e81ddb798382fddef641799bc2
lesliehealray <lesliehealray@gmail.com>
2019-11-25 22:31:07 -0500

    fixed syntax error

--- intervals/views.py
+++ intervals/views.py
@@ -26,10 +26,9 @@ def home_page(request):
 
 def progress_details(request):
     Records = Record.objects.all()
-    return render(request, "intervals/progress_details.html" {
+    return render(request, "intervals/progress_details.html", {
         'Records': Records,
     })
-
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
