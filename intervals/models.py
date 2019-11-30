c85260dfd75af472e268c1df458a5f705e4c4baa
lesliehealray <lesliehealray@gmail.com>
2019-11-30 14:58:42 -0500

    added updated model attribute for correct/incorrect guess

--- intervals/models.py
+++ intervals/models.py
@@ -6,18 +6,10 @@ class Record(models.Model):
     interval = models.CharField(max_length=30)
     guess = models.CharField(max_length=30)
     audio_url = models.URLField()
-    
+    is_correct = models.IntegerField(default=0)
     def __str__(self):
         return self.interval
-
-    @property
-    def is_correct(self):
-        return self.interval == self.guess 
-
-    
-"""
-@property decorator is just a convenient way to call the property() function, which is built in to Python.
-This function returns a special descriptor object which allows direct access to the method's computed value, 
-in this case, it is a boolean.
-"""
-
+    def save(self, *args, **kwargs):
+        if self.interval == self.guess:
+            self.is_correct = 1
+        super().save(*args, **kwargs)
