85d0a4dc98f29bb7097076199e8769e4b7c0bade
Bitha Elias <bitha.jordan@gmail.com>
2019-12-03 23:19:55 -0500

    adds final styling

--- intervals/static/main.js
+++ intervals/static/main.js
@@ -1,5 +1,5 @@
-var data1 = ['12', '19', '3', '5', '2', '3', '15', '10', '3', '7', '0', '5'];
-var data2 = ['8', '12', '3', '0', '2', '1', '10', '8', '3', '6', '0', '4'];
+var data1 = ['4', '1', '1', '5', '2', '6', '1', '1', '3', '5', '0', '4'];
+var data2 = ['0', '5', '0', '0', '2', '3', '4', '2', '3', '2', '1', '3'];
 
 function numberWrong(i) {
   return (data1[i] - data2[i])
@@ -7,47 +7,29 @@ function numberWrong(i) {
 
 
 var canvas = document.getElementById('myChart');
-canvas.height = 200;
+canvas.height = 250;
 var data = {
   labels: ['Minor 2nd', 'Major 2nd', 'Minor 3rd', 'Major 3rd', 'Perfect 4th', 'Tritone', 'Perfect 5th', 'Minor 6th', 'Major 6th', 'Minor 7th', 'Major 7th', 'Octave'],
-  datasets: [{
-    label: 'Incorrect',
-    data: data1,
-    backgroundColor: 'rgba(255, 99, 132, 0.4)',
-
-    datalabels: {
-      display: false,
+  datasets: [
+    {
+      label: 'Correct',
+      data: data1,
+      backgroundColor: 'rgba(89, 171, 227, 1)',
     },
-  }, {
-    label: 'Correct',
-    data: data2,
-    datalabels: {
-      display: true
+    {
+      label: 'Incorrect',
+      data: data2,
+      backgroundColor: 'rgba(248, 148, 6, 1)',
     },
-    backgroundColor: 'rgba(0, 255, 125, 1)',
-  },
   ]
-};
+}
 var option = {
   legend: {
     display: true,
   },
-  tooltips: {
-    //   enabled:false,
-    callback: {
-      label: function (tooltipItem, data1, data2) {
-        var label = data.datasets[tooltipItem.datasetIndex].label || '';
-        // label = (data1[i] - data2[i]);
-        label = boo;
-        return label;
-      }
-    }
-  },
-  //   tooltips: {
-  //     enabled: true,
-  //   },
   scales: {
     yAxes: [{
+      stacked: true,
       scaleFontSize: 40,
       scaleLabel: {
         display: true,
@@ -55,7 +37,6 @@ var option = {
       ticks: {
         max: 10,
         min: 0,
-        stepSize: 2,
         beginAtZero: true,
         callback: function (value) {
           return value + " "
@@ -65,12 +46,12 @@ var option = {
     xAxes: [{
       id: "bar-x-axis1",
       stacked: true,
-      barThickness: 30,
+      // barThickness: 20,
     }, {
       id: "bar-x-axis2",
       stacked: true,
       display: false,
-      barThickness: 10,
+      // barThickness: 20,
     },],
   },
   plugins: {
